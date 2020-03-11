b4382e24ac902fadd3ea240fe9fd5dc9ac4be0d0
mattted <medward4@gmail.com>
2020-03-11 15:34:56 -0700

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -17,4 +17,37 @@ class InteractiveRecord
     end
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.select{ |name| name != 'id' }.compact.join(", ")
+  end
+
+  def values_for_insert
+    self.class.column_names.map{ |name| "'#{self.send(name)}'" unless send(name).nil? }.compact.join(", ")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{self.table_name_for_insert} (#{self.col_names_for_insert}) 
+      VALUES (#{self.values_for_insert})
+    SQL
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{self.table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", name)
+  end
+
+  def self.find_by(attribute)
+    value = attribute.values[0].to_i == 0 ? "'#{attribute.values[0]}'" : attribute.values[0].to_i
+    key = attribute.keys[0].to_s
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{key} = #{value}")
+  end
+
 end
