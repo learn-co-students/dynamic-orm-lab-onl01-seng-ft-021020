b51b5f13a42f80f70bd3cd419a7a883ceef789ef
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-14 14:10:36 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
-  
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -49,14 +48,13 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
-end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
 
-def self.find_by(attribute)
-  sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = ?"
-  DB[:conn].execute(sql, attribute.values.first)
-end
-
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = ?"
+    DB[:conn].execute(sql, attribute.values.first)
+  end
 end
